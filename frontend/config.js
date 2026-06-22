1a9ba7b4c597071cc414c096f4a3c941dab72111
NETRCD STUDIO CORPORATION <neterfk@gmail.com>
2026-06-22 17:41:16 -0500

    fix: allow config.js in repo for Vercel deployment

---
+++ frontend/config.js
@@ -0,0 +1,17 @@
+// frontend/config.js
+// ⚠️ NEVER commit this file to GitHub
+
+const CONFIG = {
+  FIREBASE_URL: "https://autodesk-it-default-rtdb.firebaseio.com",
+  GROQ_API_KEY: "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",
+
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyB9tv9GMjMCYjVc5D3Y-ZDFyfmcRl9oh2c",
+    authDomain: "autodesk-it.firebaseapp.com",
+    databaseURL: "https://autodesk-it-default-rtdb.firebaseio.com",
+    projectId: "autodesk-it",
+    storageBucket: "autodesk-it.firebasestorage.app",
+    messagingSenderId: "853181134584",
+    appId: "1:853181134584:web:15640d6d5738188b732d0d",
+  },
+};
